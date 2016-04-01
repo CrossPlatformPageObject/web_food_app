a777d71d5228f81fda18346c684c080b94a1113b
avinash-pandey <pavinash@thoughtworks.com>
2016-04-01 19:01:26 +0530

    Added link for item details.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,11 @@ class ItemsController < ApplicationController
   def index
     file_path = Rails.root.join('resources','items.json').to_s
     file = File.read(file_path)
-    items = JSON.parse(file)
-    @items = items['items']
+    @@items = JSON.parse(file)
+    @items = @@items['items']
   end
+
+  def show
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  get 'items/index'
+
+  get 'items/:name' => "items#show", as: :show
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'items#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
