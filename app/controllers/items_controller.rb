552ea4b5899c810ba06ee197f4095b590d99b462
pritibiyani <priti.biyani6@gmail.com>
2016-04-03 15:54:43 +0530

    Item details will be shown when clicked on food item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,20 @@
 require 'json'
+require 'pry'
 
 class ItemsController < ApplicationController
-  def index
-    file_path = Rails.root.join('resources','items.json').to_s
-    file = File.read(file_path)
-    @@items = JSON.parse(file)
-    @items = @@items['items']
-  end
+	@@items = {}
+	def set_items
+		file_path = Rails.root.join('resources', 'items.json').to_s
+		items     = JSON.parse(File.read(file_path))
+		@@items   = items['items']
+	end
 
-  def show
-  end
+	def index
+		@items = @@items.nil? ? @items : set_items
+	end
+
+	def show
+		@item = @@items.find { |item| item['name'] == params['name'] }
+	end
 
 end
