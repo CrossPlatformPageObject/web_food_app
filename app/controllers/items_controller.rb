c7ef2a68f61cf088ef11f8b2abb32abf3b0c5eda
pritibiyani <priti.biyani6@gmail.com>
2016-04-03 21:21:24 +0530

    Cart functionality implemented

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,18 +3,38 @@ require 'pry'
 
 class ItemsController < ApplicationController
 	@@items = {}
+
 	def set_items
-		file_path = Rails.root.join('resources', 'items.json').to_s
-		items     = JSON.parse(File.read(file_path))
-		@@items   = items['items']
+		file_path        = Rails.root.join('resources', 'items.json').to_s
+		items            = JSON.parse(File.read(file_path))
+		@@items          = items['items']
+		session.delete('items')
+		session['items'] = []
 	end
 
 	def index
-		 @items = @@items.empty? ? set_items : @@items
+		@items = @@items.empty? ? set_items : @@items
 	end
 
 	def show
 		@item = @@items.find { |item| item['name'] == params['name'] }
 	end
 
+	def add
+		item = @@items.find { |item| item['name'] == params['name'] }
+		session['items'] << item
+		redirect_to items_index_path
+	end
+
+	def cart
+		@cart          = {}
+		@cart['items'] = []
+		price          = 0
+		session['items'].each do |item|
+			@cart['items'] << item
+			price += item['price']
+		end
+		@cart['total_price'] = price
+		@cart
+	end
 end
